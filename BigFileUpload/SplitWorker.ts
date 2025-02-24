7cc8f8ad2a6bacd319f5ef211df8073f3ecfc2a0
Yonjay <57708498+Mt-Youya@users.noreply.github.com>
2025-02-24 16:29:12 +0800

    Create SplitWorker.ts

---
+++ BigFileUpload/SplitWorker.ts
@@ -0,0 +1,11 @@
+import { calcChunkHash, Chunk } from "./chunk"
+
+onmessage = function (e) {
+    const chunks = e.data as Chunk[]
+    for (const chunk of chunks) {
+        calcChunkHash(chunk).then(hash => {
+            chunk.hash = hash
+            postMessage([chunk])
+        })
+    }
+}
